843ae5d0fba45554dc41d5bfa941868420ddd9df
Nate Harris <nwithan8@users.noreply.github.com>
2020-12-07 21:21:17 -0700

    Update _info.py

--- TargetAPI/_info.py
+++ TargetAPI/_info.py
@@ -1,9 +1,9 @@
-__version__ = '0.1.0'
+__version__ = '0.0.1'
 
-__title__ = "TargetAPI"
-__author__ = 'Nate Harris'
-__author_email__ = 'n8gr8gbln@gmail.com'
-__copyright__ = "Copyright © 2020 - Nate Harris"
+__title__ = "packageName"
+__author__ = 'my name'
+__author_email__ = 'myemail@example.com'
+__copyright__ = "Copyright © 2020 - my name"
 __license__ = 'GNU General Public License v3 (GPLv3)'
-__description__ = "Interact with Target's API"
-__keywords__ = ["Target", "API", "shopping", "retail", "products", "stores", "barcode", "search"]
+__description__ = "Description of the package"
+__keywords__ = ["keywords", "related", "to", "this", "package"]
