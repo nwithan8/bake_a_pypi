11e9dd92befc8a1fbba70f6046e60997206707b6
Nate Harris <nwithan8@users.noreply.github.com>
2020-12-07 21:24:44 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+import TargetAPI._info as package_info
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as fh:
+    requirements = fh.read().splitlines()
+
+setuptools.setup(
+    name=package_info.__title__,  # How you named your package folder (MyLib)
+    packages=[package_info.__title__],  # Choose the same as "name"
+    version=package_info.__version__,  # Start with a small number and increase it with every change you make
+    license=package_info.__license__,
+    description=package_info.__description__,  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=package_info.__author__,  # Type in your name
+    author_email=package_info.__author_email__,  # Type in your E-Mail
+    url=f'https://github.com/nwithan8/{package_info.__title__}',  # Provide either the link to your github or to your website
+    download_url=f'https://github.com/nwithan8/{package_info.__title__}/archive/{package_info.__version__}.tar.gz',
+    keywords=package_info.__keywords__,
+    install_requires=requirements,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 4 - Beta',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia',
+        'Topic :: Internet :: WWW/HTTP',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
