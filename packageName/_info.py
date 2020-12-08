3eebd5d1b10245822e774832e17cfb2a0f0ac9cf
Nate Harris <nwithan8@users.noreply.github.com>
2020-12-07 21:33:59 -0700

    Update _info.py

--- packageName/_info.py
+++ packageName/_info.py
@@ -3,6 +3,7 @@ __version__ = '0.0.1'
 __title__ = "packageName"
 __author__ = 'my name'
 __author_email__ = 'myemail@example.com'
+__github_username__ = "myusername"
 __copyright__ = "Copyright © 2020 - my name"
 __license__ = 'GNU General Public License v3 (GPLv3)'
 __description__ = "Description of the package"
