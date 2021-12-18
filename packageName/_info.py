d2f29dfab5e9fe80d0172d2db251d53e5674224a
Nate Harris <nwithan8@users.noreply.github.com>
2021-12-17 22:22:41 -0500

    Removed version from _info.py

--- packageName/_info.py
+++ packageName/_info.py
@@ -1,9 +1,6 @@
-__version__ = '0.0.1'
-
 __title__ = "packageName"
 __author__ = 'my name'
 __author_email__ = 'myemail@example.com'
-__github_username__ = "myusername"
 __copyright__ = "Copyright © 2021 - my name"
 __license__ = 'GNU General Public License v3 (GPLv3)'
 __description__ = "Description of the package"
